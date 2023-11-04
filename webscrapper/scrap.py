ae2530ff75b25da21345708436a90c137ea57a28
Yves Nieto <yves19161@gmail.com>
2023-11-04 14:21:37 -0400

    check

--- webscrapper/scrap.py
+++ webscrapper/scrap.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+#Gets HTML page
+url = "https://www.fisheries.noaa.gov/species/yelloweye-rockfish"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+species = doc.find_all(["div"], class_="wyswyg-edit field field--name-body field--type-text-with-summary field--label-hidden field--item")
+
+print(species)
