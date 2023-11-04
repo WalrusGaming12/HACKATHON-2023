4475ab3bf9609ef6afeae680f27f405e923f39c9
Yves Nieto <yves19161@gmail.com>
2023-11-04 19:14:11 -0400

    ADDED IMAGE GENERATOR FOR GAME LATER ON

--- webscrapper/scrap.py
+++ webscrapper/scrap.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 #Gets HTML page
 
 url="https://www.fisheries.noaa.gov/species-directory?oq=&field_species_categories_vocab=1000000031&field_region_vocab=All&items_per_page=350"
@@ -15,20 +16,41 @@ for tr in trs:
     if tr=='\n':
         next
     else:
-        name = tr.contents[3:4]
-        print(name[0].a.text)
-        print()
-
+        picture = tr.contents[1:2]
+        #get IMG
+        tag = tr.contents[3:4]
+        img=picture[0].img.get("data-src")
+        #get species name and link
+        Name=tag[0].a.text
+        href=tag[0].a.get("href")
+       
+        #store species
+        species[Name]=[href,img]
 
 """
-url = "https://www.fisheries.noaa.gov/species/adriatic-sturgeon"
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-
-species = doc.find(["h2"], class_="species-overview__header-name")
-species = species.find(["span"])
-print(list(species))
-
-description = doc.find(["div"], class_="wyswyg-edit field field--name-body field--type-text-with-summary field--label-hidden field--item")
-print(description.text)
+for i in species:
+    print("SPECIES NAME:",i)
+    print("PAGE:",species[i][0])
+    print("IMG:",species[i][1])
+    print()
 """
+#GETS DESCRIPTION
+for i in species:
+    url="https://www.fisheries.noaa.gov"+species[i][0]
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+
+    description = doc.find(["div"], class_="wyswyg-edit field field--name-body field--type-text-with-summary field--label-hidden field--item")
+    species[i].append(description.text)
+
+print(species)
+for i in species:
+    print("SPECIES NAME:",i)
+    print("PAGE:",species[i][0])
+    print("IMG:",species[i][1])
+    print("DESCRIPTIONS:")
+    print(species[i][2].encode("utf-8"))
+    print()
+
+with open('data.json', 'w') as fp:
+    json.dump(species, fp)
